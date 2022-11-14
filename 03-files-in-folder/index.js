4723aa0b8b16a0f96fd799f3b83030a258c19ff8
Nikita Berdyev <naberdyev@gmail.com>
2022-11-14 05:12:45 +0400

    refactor: use fs/promises

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,21 +1,24 @@
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
+const { stdout } = process;
 
 const secretFolderPath = path.join(__dirname, "secret-folder");
 
-fs.readdir(secretFolderPath, (error, files) => {
-  if (error) console.error(error.message);
-  else {
-    files.forEach((file) => {
-      let fileData = path.parse(path.join(secretFolderPath, `${file}`));
-      fs.stat(path.join(fileData.dir, fileData.base), (error, stats) => {
-        if (error) console.error(error.message);
-        if (stats.isFile()) {
-          console.log(
-            `${fileData.name} - ${fileData.ext.split(".")[1]} - ${stats.size}b`
-          );
-        }
-      });
-    });
+async function getFilesInfo(folder) {
+  let files = await fs.readdir(folder);
+
+  for (const file of files) {
+    let filePath = path.join(folder, file);
+    let stats = await fs.stat(filePath);
+
+    if (stats.isDirectory()) continue;
+    let fileExt = path.extname(filePath);
+    let fileName = file.split(".")[0];
+    let fileSize = stats.size;
+
+    let statsMessage = `${fileName} - ${fileExt} - ${fileSize}b\n`;
+    stdout.write(statsMessage);
   }
-});
+}
+
+getFilesInfo(secretFolderPath);
